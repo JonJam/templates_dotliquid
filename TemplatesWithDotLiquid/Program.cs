a18a7dceb41cdd40beaebd146536c4c428f4772c
Jonathan Harrison <jonjam1990@googlemail.com>
2017-10-15 20:03:42 +0100

    Example of using liquid templates

---
+++ TemplatesWithDotLiquid/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using DotLiquid;
+using DotLiquid.NamingConventions;
+using Newtonsoft.Json;
+using TemplatesWithDotLiquid.Drops;
+
+namespace TemplatesWithDotLiquid
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string dataFilePath = "./Data/ExampleData.json";
+            ExampleData data = Program.GetData(dataFilePath);
+
+            string liquidTemplateFilePath = "./Templates/ExampleTemplate.liquid";
+            Template template = Program.GetTemplate(liquidTemplateFilePath);
+
+            Hash hash = Hash.FromAnonymousObject(new { Model = data });
+
+            string renderedOutput = template.Render(hash);
+
+            Console.WriteLine(renderedOutput);
+
+            File.WriteAllText($"{Directory.GetCurrentDirectory()}/Output/RenderedTemplate.html", renderedOutput);
+        }
+
+        private static ExampleData GetData(
+           string dataFilePath)
+        {
+            string dataFileContent = File.ReadAllText(dataFilePath);
+
+            return JsonConvert.DeserializeObject<ExampleData>(dataFileContent);
+        }
+
+        private static Template GetTemplate(string templateFilePath)
+        {
+            Liquid.UseRubyDateFormat = false;
+            // Setting this means that:
+            // - Properties are accessed using CamelCase e.g. Model.PolicyNumber
+            // - Filters are accessed using CamelCase e.g. Date
+            Template.NamingConvention = new CSharpNamingConvention();
+
+            string liquidTemplateContent = File.ReadAllText(templateFilePath);
+
+            Template template = Template.Parse(liquidTemplateContent);
+            template.MakeThreadSafe();
+
+            return template;
+        }
+    }
+}
